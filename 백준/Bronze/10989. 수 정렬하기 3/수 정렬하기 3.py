8b2c82e12dbfa138536aa56248e2b4046ba80103
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-16 08:00:55 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 5564 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,8 +1,8 @@
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
 n = int(input())
 v = [0] * 10001
 for i in range(n): v[int(input())] += 1
 for i in range(1,10001):
-    iter = v[i]
-    for j in range(iter): print(i)
+    for j in range(v[i]): print(f'{i}\n')
