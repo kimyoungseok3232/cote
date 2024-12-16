26abcc4d71f32f53f1ea6a7de8462e3cb32a30f9
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-16 15:39:54 +0900

    [Silver V] Title: 단어 정렬, Time: 64 ms, Memory: 37456 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-words = set([input().strip() for _ in range(n)])
-words = sorted(list(words),key = lambda x: (len(x),x))
-print('\n'.join(words))
+words = sorted(list(set([input() for _ in range(n)])))
+words.sort(key = lambda x: (len(x),x))
+print(''.join(words))
