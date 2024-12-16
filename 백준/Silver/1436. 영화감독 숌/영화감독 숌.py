8b3e045a92914b8ec8841ea08c12af90eda5427b
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-16 13:16:13 +0900

    [Silver V] Title: 영화감독 숌, Time: 48 ms, Memory: 33948 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,9 +1,5 @@
 n = int(input())
-c = set([666])
-for i in range(3000):
-    t = str(i)
-    for j in range(len(t)+1):
-        c.add(int(t[:j]+'666'+t[j:]))
+c = set([666]+[int(str(i)[:j]+'666'+str(i)[j:]) for i in range(min(2800,n)) for j in range(len(str(i))+1)])
 for i in range(100): c.add(int('6660'+str(i)))
 for i in range(10): c.add(int('66600'+str(i)))
 c = sorted(list(c))
