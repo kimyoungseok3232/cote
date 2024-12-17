a01e7aa42b2f2374f09249e4d1cb24291883631f
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-17 19:19:56 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+s = deque()
+for i in range(n):
+    c = input().strip().split()
+    if  c[0] == 'push': s.append(c[1])
+    elif c[0] == 'pop': print(s.popleft() if s else -1)
+    elif c[0] == 'size': print(len(s))
+    elif c[0] == 'empty': print(0 if s else 1)
+    elif c[0] == 'front': print(s[0] if s else -1)
+    elif c[0] == 'back': print(s[-1] if s else -1)
