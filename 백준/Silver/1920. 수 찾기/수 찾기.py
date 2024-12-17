1c76230d6749291ec6b4dab47e0d7fb4b1216d3d
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-17 17:06:18 +0900

    [Silver IV] Title: 수 찾기, Time: 88 ms, Memory: 52264 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,4 +1,4 @@
 n, nlist = input(), set(input().split())
-m, mlist = input(), input().split()
-res = '\n'.join(['1' if num in nlist else '0' for num in mlist])
+m = input()
+res = '\n'.join(['1' if num in nlist else '0' for num in input().split()])
 print(res)
