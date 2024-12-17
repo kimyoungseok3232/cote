56e07fed49888700065a00c42a8f9c0925c7c011
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-17 18:32:59 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 608 ms, Memory: 155876 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,7 +1,5 @@
-import sys
 from collections import defaultdict
-print = sys.stdout.write
 n, d = input(), defaultdict(int)
 for i in input().split(): d[i] += 1
 m = input()
-for i in input().split(): print(f'{d[i]}\n')
+print(" ".join(map(str, (d[i] for i in input().split()))))
