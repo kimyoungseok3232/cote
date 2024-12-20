cff8d0c8a8b264e5bb12bdc074bea3a37786d4a5
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-21 06:30:39 +0900

    [Gold V] Title: 회의실 배정, Time: 208 ms, Memory: 48072 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -5,8 +5,8 @@ t = [tuple(map(int, input().split())) for _ in range(n)]
 t.sort(key=lambda x: x[0])
 t.sort(key=lambda x: x[1])
 lt, c = 0, 0
-for i in range(n):
-    if lt > t[i][0]: continue
+for i in t:
+    if lt > i[0]: continue
     c += 1
-    lt = t[i][1]
+    lt = i[1]
 print(c)
