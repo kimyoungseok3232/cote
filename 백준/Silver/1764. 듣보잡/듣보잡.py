0d7c54434e85d95d3adff3738cf642f3dcf07f15
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-21 02:43:04 +0900

    [Silver IV] Title: 듣보잡, Time: 64 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,8 +2,6 @@ import sys
 input = sys.stdin.readline
 n, m = map(int,input().split())
 nl = set(input() for _ in range(n))
-res = []
-for _ in range(m):
-    if (i:=input()) in nl: res.append(i)
+res = [i for _ in range(m) if (i := input()) in nl]
 print(len(res))
 print(''.join(sorted(res)))
