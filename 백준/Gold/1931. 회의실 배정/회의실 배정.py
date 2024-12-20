f198e54434da8eabc3c5ff002cc04fb5679eca3c
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-21 06:25:07 +0900

    [Gold V] Title: 회의실 배정, Time: 240 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,7 +1,8 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-t = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x: (x[1], x[0]))
+t = [tuple(map(int, input().split())) for _ in range(n)]
+t.sort(key=lambda x: (x[1], x[0]))
 res = [t[0]]
 for i in range(1,n):
     if res[-1][1] <= t[i][0]: res.append(t[i])
