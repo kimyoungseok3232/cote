8ddf756f6b1bdefcba006b0e67493264e377e907
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-24 01:43:26 +0900

    [Gold V] Title: AC, Time: 84 ms, Memory: 41816 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,9 +1,9 @@
 for _ in range(int(input())):
-    c, n = input().split('R'), input()
-    l = eval(input()) + [0]
+    c, n = input().split('R'), int(input())
+    l = input().strip('[]').split(',') + [0]
     st, ed = len(''.join(c[::2])), -1-len(''.join(c[1::2]))
-    if st-ed > len(l): print('error')
+    if st-ed > n+1: print('error')
     else: 
         l = l[st:ed]
         if not len(c)%2: l.reverse()
-        print(f'{l}'.replace(' ', ''))
+        print(f'[{','.join(l)}]')
