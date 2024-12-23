14f3ceac26d42d17144b7bc5cc5d0c3e4b9c816c
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-23 23:41:30 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 208 ms, Memory: 55964 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -5,7 +5,7 @@ n, m = map(int, input().split())
 d = {}
 for i in range(1,n+1):
     p = input().strip()
-    d[p.lower()], d[f'{i}'] = i, p
+    d[p], d[f'{i}'] = i, p
 for i in range(m):
     q = input().strip()
-    print(f'{d[q.lower()]}\n')
+    print(f'{d[q]}\n')
