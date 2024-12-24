3b420c0a9b07e5a93d684e0abe17010e1cf1e14f
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-24 09:31:43 +0900

    [Silver II] Title: 최소 힙, Time: 92 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,6 +1,7 @@
 import heapq, sys
 input = sys.stdin.readline
+print = sys.stdout.write
 h = []
 for _ in range(int(input())):
     if (i:=int(input())): heapq.heappush(h, i)
-    else: print(heapq.heappop(h) if h else 0)
+    else: print(f'{heapq.heappop(h)}\n' if h else '0\n')
