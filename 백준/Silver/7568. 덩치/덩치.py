4ebd3efd74f2efe1d301807251879bfa6d39c6ad
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-25 00:20:57 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,8 +1,8 @@
 n = int(input())
 l = [tuple(map(int, input().split())) for _ in range(n)]
 d = {}
-for i in range(n):
+for i in l:
     c = 1
-    for j in range(n):
-        if l[i][0] < l[j][0] and l[i][1] < l[j][1]: c+=1
+    for j in l:
+        if i[0] < j[0] and i[1] < j[1]: c+=1
     print(c, end=' ')
