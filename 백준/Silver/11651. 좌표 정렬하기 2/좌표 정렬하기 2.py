c734a4273c4c1ef7999ec051b4bb421afc6f3072
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-25 00:50:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 180 ms, Memory: 55000 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,5 +1,3 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-p = sorted([input() for _ in range(n)], key = lambda x: (int(x.split()[1]), int(x.split()[0])))
-print(''.join(p))
+print(''.join(sorted([input() for _ in range(int(input()))], key = lambda x: (int(x.split()[1]), int(x.split()[0])))))
