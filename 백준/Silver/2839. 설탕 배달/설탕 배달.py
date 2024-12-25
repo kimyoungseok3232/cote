1c4b9c7337a8566a842781dbd9670fe756353ec0
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-25 21:45:25 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,6 +1,7 @@
 n = int(input())
-if n%5 == 0: print(n//5)
-elif n%5 == 1: print(2+(n-6)//5)
-elif n%5 == 2: print(4+(n-12)//5 if n > 11 else -1)
-elif n%5 == 3: print(1+(n-3)//5)
-elif n%5 == 4: print(3+(n-9)//5 if n > 8 else -1)
+if n%5 == 0: n//=5
+elif n%5 == 1: n = 2+(n-6)//5
+elif n%5 == 2: n = 4+(n-12)//5 if n > 11 else -1
+elif n%5 == 3: n = 1+(n-3)//5
+elif n%5 == 4: n = 3+(n-9)//5 if n > 8 else -1
+print(n)
