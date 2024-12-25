b3aba3b63330b1f411e0cb5f885abd633f0d0aab
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-25 21:38:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,6 @@
+n = int(input())
+if n%5 == 0: print(n//5)
+elif n%5 == 1: print(2+(n-6)//5)
+elif n%5 == 2: print(4+(n-12)//5 if n > 11 else -1)
+elif n%5 == 3: print(1+(n-3)//5)
+elif n%5 == 4: print(3+(n-9)//5 if n > 8 else -1)
