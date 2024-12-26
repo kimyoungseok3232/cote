ea55a0439fd6005c8ebe2f6b23e078829ea6e994
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 01:33:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,8 +1,8 @@
 import sys
 input = sys.stdin.readline
+print = sys.stdout.write
 while (st:=input().rstrip()) != '.':
     st = ''.join(filter(lambda x: x in ('[',']','(',')','.'), st))
-    while '[]' in st or '()' in st:
-        st = st.replace('[]','').replace('()','')
-    if st == '.': print('yes')
-    else: print('no')
+    while '[]' in st or '()' in st: st = st.replace('[]','').replace('()','')
+    if st == '.': print('yes\n')
+    else: print('no\n')
