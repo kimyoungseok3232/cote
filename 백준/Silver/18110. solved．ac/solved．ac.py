ca3ea89d19694a7380520f04e2e0f20bb4ce790b
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 02:11:21 +0900

    [Silver IV] Title: solved.ac, Time: 140 ms, Memory: 39000 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,7 @@
+import sys
+ip = sys.stdin.readline
+if n:=int(ip()):
+    l = sorted([int(ip()) for _ in range(n)])
+    if r:=int(0.5+n*0.15): l = l[r:-r]
+    print(int(0.5+sum(l)/(n-2*r)))
+else: print(0)
