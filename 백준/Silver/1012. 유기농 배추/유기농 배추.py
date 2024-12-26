283b7c38abe32f1a9ba06ad230a506f13fb8f664
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 00:36:27 +0900

    [Silver II] Title: 유기농 배추, Time: 212 ms, Memory: 33864 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(10**6)
+def dfs(x,y,d):
+    if (x,y) in d: del d[(x,y)]
+    for dx, dy in [(0,1),(0,-1),(1,0),(-1,0)]:
+        nx, ny = x+dx, y+dy
+        if (nx, ny) in d: dfs(nx,ny,d)
+for _ in range(int(input())):
+    w, h, k = map(int, input().split())
+    d = {tuple(map(int, input().split())): 1 for _ in range(k)}
+    r = 0
+    while d:
+        dfs(*list(d.keys())[0],d)
+        r += 1
+    print(r)
