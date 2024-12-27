bbd52b130594801f803e0a0eb60d3fdc0013201b
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 20:50:48 +0900

    [Silver III] Title: 통계학, Time: 344 ms, Memory: 59588 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,20 +1,9 @@
 import sys
+from collections import Counter as C
 ip = sys.stdin.readline
-n = int(ip())
-l, c, d, s = [], [], {}, 0
-mn, mx, md, ct = 4000, -4000, 0, 0
-for _ in range(n):
-    v = int(ip())
-    s+=v
-    l.append(v)
-    if v < mn: mn = v
-    if v > mx: mx = v
-    if v not in d: d[v] = 0
-    d[v] += 1
-    if ct < d[v]: ct, c = d[v], [v]
-    elif ct == d[v]: c.append(v)
-l.sort(), c.sort()
-print(int(s/n+0.5) if s>0 else int(s/n-0.5))
-print(l[n//2])
-print(c[1] if len(c)>1 else c[0])
-print(mx-mn)
+l = sorted([int(ip()) for _ in range(int(ip()))])
+f = C(l).most_common(2)
+print(int(m+0.5) if (m:=sum(l)/len(l))>0 else int(m-0.5))
+print(l[len(l)//2])
+print(f[1][0] if len(f)>1 and f[0][1] == f[1][1] else f[0][0])
+print(l[-1]-l[0])
