d51272b2311967e5fe6fa382b8f7a98dce73c7c7
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 17:39:00 +0900

    [Silver III] Title: 소수 구하기, Time: 256 ms, Memory: 47760 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,6 +4,4 @@ for i in range(2, int(n**(1/2))+1):
     if t[i]:
         for j in range(i*i, n+1, i):
             t[j] = False
-
-for i in range(m, n+1):
-    if t[i]: print(i)
+print('\n'.join([f'{i}' for i in range(m, n+1) if t[i]]))
