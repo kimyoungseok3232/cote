c2a66d143ae1102d754f484a9b8ab90f42b88f55
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 20:29:49 +0900

    [Silver III] Title: 통계학, Time: 576 ms, Memory: 53796 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,9 +1,20 @@
 import sys
-from collections import Counter as C
 ip = sys.stdin.readline
-l = sorted([int(ip()) for _ in range(int(ip()))])
-f = C(l)
-print(int(m+0.5) if (m:=sum(l)/len(l))>0 else int(m-0.5))
-print(l[len(l)//2])
-print(i[1] if len(i:=[k for k, v in f.items() if v == max(f.values())]) > 1 else i[0])
-print(max(l)-min(l))
+n = int(ip())
+l, c, d, s = [], [], {}, 0
+mn, mx, md, ct = 4000, -4000, 0, 0
+for _ in range(n):
+    v = int(ip())
+    s+=v
+    l.append(v)
+    if v < mn: mn = v
+    if v > mx: mx = v
+    if v not in d: d[v] = 0
+    d[v] += 1
+    if ct < d[v]: ct, c = d[v], [v]
+    elif ct == d[v]: c.append(v)
+l.sort(), c.sort()
+print(int(s/n+0.5) if s>0 else int(s/n-0.5))
+print(l[n//2])
+print(c[1] if len(c)>1 else c[0])
+print(mx-mn)
