c05da883c80c671839acd9f981313f79a89926f5
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 17:36:05 +0900

    [Silver III] Title: 소수 구하기, Time: 292 ms, Memory: 47760 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,22 +1,9 @@
-import sys
-print = sys.stdout.write
 m, n = map(int, input().split())
-t = []
-for i in range(2, m):
-    for j in t: 
-        if i**(1/2) < j:
-            t.append(i)
-            break
-        elif i % j == 0: break
-    else: t.append(i)
+t = [False, False]+[True]*(n-1)
+for i in range(2, int(n**(1/2))+1):
+    if t[i]:
+        for j in range(i*i, n+1, i):
+            t[j] = False
 
-for i in range(max(2,m), n+1):
-    for j in t: 
-        if i**(1/2) < j:
-            t.append(i)
-            print(f'{i}\n')
-            break
-        elif i % j == 0: break
-    else: 
-        t.append(i)
-        print(f'{i}\n')
+for i in range(m, n+1):
+    if t[i]: print(i)
