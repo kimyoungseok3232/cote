326f6091a258cc8772db6faea8cc57bb51aefcd1
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 21:07:34 +0900

    [Silver III] Title: 통계학, Time: 224 ms, Memory: 44076 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,9 +1,9 @@
 import sys
 from collections import Counter as C
 ip = sys.stdin.readline
-l = sorted([int(ip()) for _ in range(int(ip()))])
-f = C(l).most_common(2)
+f = C(int(ip()) for i in range(int(ip())))
+l = sorted(list(f.elements()))
 print(int(m+0.5) if (m:=sum(l)/len(l))>0 else int(m-0.5))
 print(l[len(l)//2])
-print(f[1][0] if len(f)>1 and f[0][1] == f[1][1] else f[0][0])
+print(f[1][0] if len((f:=sorted(f.items(), key=lambda x: (-x[1], x[0]))))>1 and f[0][1] == f[1][1] else f[0][0])
 print(l[-1]-l[0])
