883ce3c07a4edd43be10ea6e9f0035b281e7a441
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-27 18:28:52 +0900

    [Silver III] Title: 프린터 큐, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,16 @@
+from collections import deque
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    q = deque(map(int, input().split()))
+    r = 1
+    v = q.index(max(q))
+    m = (m-v)%len(q)
+    q.rotate(-v)
+    q.popleft()
+    while m: 
+        r += 1
+        v = q.index(max(q))
+        m = (m-1-v)%len(q)
+        q.rotate(-v)
+        q.popleft()
+    print(r)
