def650f0b298d1d2360a631273a245b3b1640394
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2024-12-28 21:14:22 +0900

    [Silver II] Title: 스택 수열, Time: 108 ms, Memory: 39268 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,10 +2,9 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 s = [int(input()) for _ in range(n)]
-t = [i for i in range(1,n+1)]
 r, a, c = [], [], 0
-for i in range(n):
-    r.append(t[i])
+for i in range(1, n+1):
+    r.append(i)
     a.append('+')
     while r and r[-1] == s[c]:
         r.pop()
