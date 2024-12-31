f3530c75ea352373e6313e30027513f4720e234b
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-01 04:52:33 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,9 +4,7 @@ k, n = map(int, input().split())
 l = [int(input()) for _ in range(k)]
 mx, mn = sum(l)//n, 0
 while mn <= mx:
-    res = 0
     m = (mn+mx)//2
-    for i in l: res += i//(m or 1)
-    if res < n: mx = m-1
+    if sum(i//(m or 1) for i in l) < n: mx = m-1
     else: mn = m+1
 print(mx)
