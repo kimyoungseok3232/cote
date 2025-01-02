24370717e803b30f4817694cf387710327212e6a
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-03 00:57:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -3,6 +3,5 @@ l = {0:0}
 for i in map(int, input().split()):
     for j in list(l.keys()):
         if j < i : 
-            if i in l and l[i] > l[j]: continue
-            l[i] = l[j]+1
+            if not (i in l and l[i] > l[j]): l[i] = l[j]+1
 print(max(l.values()))
