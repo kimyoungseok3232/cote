3fb8da333bc35f158973d36ac801347160ddf381
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-02 20:52:29 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 584 ms, Memory: 45884 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,11 +1,12 @@
 n, m = map(int, input().split())
-l = [input().split() for _ in range(n)]
+l = [input().replace('1', '-1').split() for _ in range(n)]
 for i in range(n):
     for j in range(m): 
-        if l[i][j] == '2': break
+        if l[i][j] == '2': 
+            l[i][j] = '0'
+            break
     else: continue
     break
-r = [['-1' if l[i][j] == '1' else '0' for j in range(m)] for i in range(n)]
 c, nn = 1, [(i, j)]
 while nn:
     tn = []
@@ -14,10 +15,10 @@ while nn:
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx, ny = x + dx, y + dy
             if nx < 0 or ny < 0 or nx > n-1 or ny > m-1: continue
-            if r[nx][ny] == '-1':
-                r[nx][ny] = f'{c}'
+            if l[nx][ny] == '-1':
+                l[nx][ny] = f'{c}'
                 tn.append((nx, ny))
   
     nn = tn
     c += 1
-for i in r: print(' '.join(i))
+for i in l: print(' '.join(i))
