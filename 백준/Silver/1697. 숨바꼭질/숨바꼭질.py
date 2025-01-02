b7f7a682eb9fa6ef780229611d8e2533b0d2bebe
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-02 23:35:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,12 +1,12 @@
 from collections import deque
 n, k = map(int, input().split())
-v = [True] * 100001
+v = [1] * 100001
 q = deque([(n, 0)])
 while q:
     t, c = q.popleft()
     if t == k: break
     for i in [t*2, t+1, t-1]:
         if -1 < i < 100001 and v[i]:
-            v[i] = False
+            v[i] = 0
             q.append((i, c+1))
 print(c)
