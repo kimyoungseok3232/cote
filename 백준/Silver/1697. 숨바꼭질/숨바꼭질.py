b7e17a33defdd5b7bb896ef4307e1afb4deae2a8
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-02 23:34:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -5,7 +5,7 @@ q = deque([(n, 0)])
 while q:
     t, c = q.popleft()
     if t == k: break
-    for i in [t+1, t*2, t-1]:
+    for i in [t*2, t+1, t-1]:
         if 0 <= i <= 100000 and v[i]:
             v[i] = False
             q.append((i, c+1))
