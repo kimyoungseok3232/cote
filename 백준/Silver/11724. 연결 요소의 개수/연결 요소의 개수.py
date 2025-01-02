86a65c5965384288eef9df48bfaf54840947e445
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-02 21:54:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 828 ms, Memory: 89672 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,22 +1,20 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-def dfs(x):
-    while x in l and l[x]:
-        v = l[x].pop()
-        l[v].discard(x)
-        dfs(v)
-    if x in l: del l[x]
 n, m = map(int, input().split())
-l = {i+1:set() for i in range(n)}
+l = [set() for i in range(n+1)]
+v = set(i for i in range(1, n+1))
 for i in range(m):
     a, b = map(int, input().split())
     l[a].add(b)
     l[b].add(a)
 c = 0
-while l:
-    for i in l:
-        dfs(i)
-        break
+while v:
+    nn = [v.pop()]
+    while nn:
+        x = nn.pop()
+        for y in l[x]:
+            if y in v:
+                v.remove(y)
+                nn.append(y)
     c += 1
 print(c)
