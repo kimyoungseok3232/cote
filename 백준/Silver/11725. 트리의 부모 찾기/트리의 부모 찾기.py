8fce2870eb7b600a438bf8bf1f67b3a457664c1f
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-03 21:49:49 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 364 ms, Memory: 73188 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+l = [set() for _ in range(n+1)]
+for i in range(n-1):
+    a, b = map(int, input().split())
+    l[a].add(b)
+    l[b].add(a)
+t = [1]
+v = set(t)
+p = [0] * (n+1)
+while t:
+    x = t.pop()
+    while l[x]:
+        y = l[x].pop()
+        if y in v: continue
+        p[y] = x
+        t.append(y)
+        v.add(y)
+print(*p[2:])
