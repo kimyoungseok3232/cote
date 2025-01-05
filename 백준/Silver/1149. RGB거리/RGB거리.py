265aa5bab8275c89043520ed8f9cf467a2683730
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 08:16:50 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
-v = [0, 0, 0]
+mr, mg, mb = 0, 0, 0
 for _ in range(int(input())):
     r, g, b = map(int, input().split())
-    v[0], v[1], v[2] = min(v[1], v[2]) + r, min(v[0], v[2]) + g, min(v[0], v[1]) + b
-print(min(v))
+    mr, mg, mb = min(mg, mb) + r, min(mr, mb) + g, min(mr, mg) + b
+print(min(mr, mg, mb))
