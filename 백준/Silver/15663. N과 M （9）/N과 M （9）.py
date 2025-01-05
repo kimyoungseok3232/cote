038f8022d19549a80a59e1d17e81f0761245e9f0
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 02:59:03 +0900

    [Silver II] Title: N과 M (9), Time: 52 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,7 +1,9 @@
+import sys
 from itertools import permutations
+print = sys.stdout.write
 n, m = map(int, input().split())
 v = set()
 for i in permutations(sorted(input().split(), key = int), m): 
-    if (st:=' '.join(i)) in v: continue
+    if (st:=' '.join(i)+'\n') in v: continue
     print(st)
     v.add(st)
