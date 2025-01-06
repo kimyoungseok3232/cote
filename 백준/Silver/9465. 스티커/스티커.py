3d79b58f6b98c9f25e139afee038c8de04c323f0
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 19:08:31 +0900

    [Silver I] Title: 스티커, Time: 564 ms, Memory: 43772 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline
 for _ in range(int(input())):
-    n = int(input())
-    u = list(map(int,input().split()))
-    d = list(map(int,input().split()))
+    n = input()
     v = [0, 0, 0, 0]
-    for i in range(n):
+    for i in zip(map(int,input().split()),map(int,input().split())):
         tmp1, tmp2 = v[0], v[1]
-        v[0], v[1] = max(v[1], v[3]) + u[i], max(v[0], v[2]) + d[i]
+        v[0], v[1] = max(v[1], v[3]) + i[0], max(v[0], v[2]) + i[1]
         v[2], v[3] = tmp1, tmp2
     print(max(v))
