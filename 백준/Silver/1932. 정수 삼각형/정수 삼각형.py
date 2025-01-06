d7dd573c255da9f075315ac8dba0c8e7df79a056
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 09:52:53 +0900

    [Silver I] Title: 정수 삼각형, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-l = [0]
-for _ in range(n):
+l = [int(input())]
+for _ in range(n-1):
     t = list(map(int, input().split()))
-    for i in range(len(t)):
-        if i == 0: t[i] = l[i] + t[i]
-        elif i == len(t)-1: t[i] = l[i-1] + t[i]
-        else: t[i] = max(l[i], l[i-1]) + t[i]
+    t[0] += l[0]
+    for i in range(1,len(t)-1):
+        t[i] += max(l[i], l[i-1])
+    t[-1] += l[-1]
     l = t
 print(max(l))
