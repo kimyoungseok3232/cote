2518628baceeb67fa4c3c613386d76f0a0c7d7cb
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 09:43:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+n = int(input())
+l = [0] * n
+for _ in range(n):
+    t = list(map(int, input().split()))
+    tl = l.copy()
+    for i in range(len(t)):
+        if i == 0: tl[i] = l[i] + t[i]
+        elif i == len(t)-1: tl[i] = l[i-1] + t[i]
+        else: tl[i] = max(l[i], l[i-1]) + t[i]
+    l = tl
+print(max(l))
