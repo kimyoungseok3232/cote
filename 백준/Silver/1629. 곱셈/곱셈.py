22e29cf266e7850ace80657e0b6de94a235b1cd4
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-06 09:23:22 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,7 +1,8 @@
 a,b,c = map(int, input().split())
+l = [a]
+for i in range(31): l.append(l[i]**2%c)
+b = f'{b:b}'
 r = 1
-while b:
-    if b%2: r = r*a%c
-    a = a*a%c
-    b //= 2
-print(r)
+for i in range(len(b)): 
+    if b[-i-1] == '1': r = r*l[i]%c
+print(r%c)
