c18fc64f24efb39c403cc84eeb44c3a6039e62fb
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-07 15:38:15 +0900

    [Gold V] Title: LCS, Time: 192 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -4,8 +4,6 @@ dp = [0] * (len(str2)+1)
 for i in str1:
     tmp = 0
     for j in range(len(str2)):
-        if tmp < dp[j]:
-            tmp = dp[j]
-            continue
-        if i == str2[j]: dp[j] = tmp+1
+        if tmp < dp[j]: tmp = dp[j]
+        elif i == str2[j]: dp[j] = tmp+1
 print(max(dp))
