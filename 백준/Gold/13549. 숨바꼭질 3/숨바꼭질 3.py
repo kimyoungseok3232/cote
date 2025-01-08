4442b78be612e792eb39ccc987d13eaaf239db8b
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-09 04:20:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 35300 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -8,7 +8,7 @@ while q:
         if t == k: break
         v[t] = 0
         for i in [t+1, t-1]:
-            if -1 < i < 100001 and v[i] == 2:
+            if -1 < i < 100001 and v[i] > 1:
                 v[i] = 1
                 q.append((i, c+1))
         t *= 2
