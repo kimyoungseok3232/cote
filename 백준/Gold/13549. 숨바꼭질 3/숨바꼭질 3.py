518169e59bfd816fb932b2e0cd1d6215880b4a2d
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-09 03:55:24 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 36024 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -5,16 +5,15 @@ vd = [1] * 100001
 q = deque([(n, 0)])
 while q:
     t, c = q.popleft()
-    tmp = t
-    while tmp < 100001 and vd[tmp]:
-        if tmp == k: break
-        v[tmp] = 0
-        vd[tmp] = 0
-        for i in [tmp+1, tmp-1]:
+    while t < 100001 and vd[t]:
+        if t == k: break
+        v[t] = 0
+        vd[t] = 0
+        for i in [t+1, t-1]:
             if -1 < i < 100001 and v[i]:
                 v[i] = 0
                 q.append((i, c+1))
-        tmp *= 2
+        t *= 2
     else: continue
     break
 print(c)
