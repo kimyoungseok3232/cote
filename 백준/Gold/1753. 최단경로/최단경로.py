2409e6046d61433115b1f5871b0314d2103ece7a
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-09 10:40:08 +0900

    [Gold IV] Title: 최단경로, Time: 640 ms, Memory: 69300 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,6 +1,7 @@
 import sys
 from heapq import heappush, heappop
 input = sys.stdin.readline
+print = sys.stdout.write
 n, edge = map(int, input().split())
 start = int(input())
 graph = [[] for _ in range(n+1)]
@@ -8,7 +9,7 @@ for i in range(edge):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
 
-dist = [float('INF')] * (n + 1)
+dist = [10e9] * (n + 1)
 dist[start] = 0
 heap = [(0, start)]
 
@@ -20,4 +21,6 @@ while heap:
             dist[v] = current_dist + w
             heappush(heap, (dist[v], v))
 
-print(*map(lambda x: f'{x:.0F}',dist[1:]))
+for i in range(1, n+1):
+    if dist[i] == 10e9: print('INF\n')
+    else: print(f'{dist[i]}\n')
