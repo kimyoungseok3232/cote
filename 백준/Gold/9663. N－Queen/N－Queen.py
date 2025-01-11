0c1d8b66b3f75d9a7b5e6139e46c50ffaa7bcf1a
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-12 05:34:05 +0900

    [Gold IV] Title: N-Queen, Time: 12040 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,17 +1,16 @@
-def n_queen(n):
-    def solve(row, cols, diags1, diags2):
-        if row == n: return 1
-        count = 0
-        available = ((1 << n) - 1) & ~(cols | diags1 | diags2)
-        while available:
-            pos = available & -available
-            available -= pos
-            count += solve(
-                row + 1,
-                cols | pos,
-                (diags1 | pos) << 1,
-                (diags2 | pos) >> 1
-            )
-        return count
-    return solve(0, 0, 0, 0)
-print(n_queen(int(input())))
+n = int(input())
+def n_queen(row, cols, diags1, diags2):
+    if row == n: return 1
+    count = 0
+    available = ((1 << n) - 1) & ~(cols | diags1 | diags2)
+    while available:
+        pos = available & -available
+        available -= pos
+        count += n_queen(
+            row + 1,
+            cols | pos,
+            (diags1 | pos) << 1,
+            (diags2 | pos) >> 1
+        )
+    return count
+print(n_queen(0, 0, 0, 0))
