e3bfc4d7a80941c77091d39feb96a1515b26d966
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-16 22:45:56 +0900

    [Gold IV] Title: 플로이드, Time: 504 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,15 @@
 import sys
-from heapq import heappop, heappush
 input = sys.stdin.readline
 n, edge = int(input()), int(input())
-graph = [[] for _ in range(n+1)]
+graph = [[10e8 if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
 for i in range(edge):
     u, v, w = map(int, input().split())
-    graph[u].append((v, w))
+    graph[u][v] = min(graph[u][v], w)
 
-for start in range(1, n+1):
-    dist = [10e9] * (n + 1)
-    dist[start] = 0
-    heap = [(0, start)]
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-    while heap:
-        current_dist, node = heappop(heap)
-        if current_dist > dist[node]: continue
-        for v, w in graph[node]:
-            if current_dist + w < dist[v]:
-                dist[v] = current_dist + w
-                heappush(heap, (dist[v], v))
-
-    print(*map(lambda x: 0 if x == 10e9 else x, dist[1:]))
+for i in range(1, n+1):
+    print(*map(lambda x: 0 if x == 10e8 else x, graph[i][1:]))
