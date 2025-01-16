d95fe13194f0a48cd447e0e9481f1a198e1e0701
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-16 22:50:11 +0900

    [Gold IV] Title: 플로이드, Time: 228 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,15 +1,17 @@
 import sys
 input = sys.stdin.readline
 n, edge = int(input()), int(input())
-graph = [[10e8 if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
-for i in range(edge):
-    u, v, w = map(int, input().split())
-    graph[u][v] = min(graph[u][v], w)
+def solve(n,edge):
+    graph = [[10e8 if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
+    for i in range(edge):
+        u, v, w = map(int, input().split())
+        graph[u][v] = min(graph[u][v], w)
 
-for k in range(1, n+1):
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
     for i in range(1, n+1):
-        for j in range(1, n+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-for i in range(1, n+1):
-    print(*map(lambda x: 0 if x == 10e8 else x, graph[i][1:]))
+        print(*map(lambda x: 0 if x == 10e8 else x, graph[i][1:]))
+solve(n, edge)
