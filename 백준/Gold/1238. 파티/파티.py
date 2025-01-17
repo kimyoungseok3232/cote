bf150bbfb640eff76a988b361a598677bce9bd17
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-18 02:59:23 +0900

    [Gold III] Title: 파티, Time: 1440 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,28 @@
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+n, edge, end = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for i in range(edge):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+to_end = [0] * (n + 1)
+
+for start in range(1, n+1):
+    dist = [10e9] * (n + 1)
+    dist[start] = 0
+    heap = [(0, start)]
+
+    while heap:
+        current_dist, node = heappop(heap)
+        if current_dist > dist[node]: continue
+        for v, w in graph[node]:
+            if current_dist + w < dist[v]:
+                dist[v] = current_dist + w
+                heappush(heap, (dist[v], v))
+
+    if start == end: end_to = dist
+    else: to_end[start] = dist[end]
+    
+print(max(map(lambda x: end_to[x]+to_end[x], range(1, n+1))))
