d823fa2a91bfb6d0909b073670aaab62410aa0e0
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-18 03:12:20 +0900

    [Gold III] Title: 파티, Time: 44 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,26 +4,33 @@ input = sys.stdin.readline
 def solve():
     n, edge, end = map(int, input().split())
     graph = [[] for _ in range(n+1)]
+    reverse_graph = [[] for _ in range(n+1)]
     for i in range(edge):
         u, v, w = map(int, input().split())
         graph[u].append((v, w))
+        reverse_graph[v].append((u, w))
 
-    to_end = [0] * (n + 1)
 
-    for start in range(1, n+1):
-        dist = [10e9] * (n + 1)
-        dist[start] = 0
-        heap = [(0, start)]
+    dist = [10e9] * (n + 1)
+    dist[end] = 0
+    heap = [(0, end)]
+    while heap:
+        current_dist, node = heappop(heap)
+        if current_dist > dist[node]: continue
+        for v, w in graph[node]:
+            if current_dist + w < dist[v]:
+                dist[v] = current_dist + w
+                heappush(heap, (dist[v], v))
 
-        while heap:
-            current_dist, node = heappop(heap)
-            if current_dist > dist[node]: continue
-            for v, w in graph[node]:
-                if current_dist + w < dist[v]:
-                    dist[v] = current_dist + w
-                    heappush(heap, (dist[v], v))
-
-        if start == end: end_to = dist
-        else: to_end[start] = dist[end]    
-    print(max(end_to[x]+to_end[x] for x in range(1, n+1)))
+    reverse_dist = [10e9] * (n + 1)
+    reverse_dist[end] = 0
+    heap = [(0, end)]
+    while heap:
+        current_dist, node = heappop(heap)
+        if current_dist > reverse_dist[node]: continue
+        for v, w in reverse_graph[node]:
+            if current_dist + w < reverse_dist[v]:
+                reverse_dist[v] = current_dist + w
+                heappush(heap, (reverse_dist[v], v))
+    print(max(map(lambda x: dist[x]+reverse_dist[x], range(1, n+1))))
 solve()
