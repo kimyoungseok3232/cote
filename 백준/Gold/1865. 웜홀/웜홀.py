df6db81f5503db543c43d3ef5ce21bde3e5a33ae
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-23 23:50:04 +0900

    [Gold III] Title: 웜홀, Time: 152 ms, Memory: 36336 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
-def find_minus_loop(node, edges, dist):    
-    for i in range(node):
-        for st, ed, w in edges:
-            if dist[st] + w < dist[ed]:
-                dist[ed] = dist[st] + w
-                if i == node-1: 
-                    return 'YES'
-    return 'NO'
-
 def solve(n):
-    for i in range(n):
+    for _ in range(n):
         node, edge, hole = map(int, input().split())
-        edges = []
-        dist = [10e8] * (node+1)
-        dist[1] = 0
+        edges = [[] for _ in range(node+1)]
+        graph = [[0]*(node+1) for _ in range(node+1)]
+        wh_st = set()
         for _ in range(edge):
             s, e, t = map(int, input().split())
-            edges.append((s,e,t))
-            edges.append((e,s,t))
+            edges[s].append((e,t))
+            edges[e].append((s,t))
         for _ in range(hole):
             s, e, t = map(int, input().split())
-            edges.append((s,e,-t))
-        
-        print(find_minus_loop(node, edges, dist))
+            edges[s].append((e,-t))
+            graph[s][e] = -t
+            wh_st.add((s,e))
+
+        while wh_st:
+            st, mid = wh_st.pop()
+            for ed, time in edges[mid]:
+                if graph[st][mid] + time < graph[st][ed]:
+                    graph[st][ed] = graph[st][mid] + time
+                    wh_st.add((st, ed))
+                    if st == ed:
+                        print('YES')
+                        break
+            else: continue
+            break
+        else: print('NO')
 solve(int(input()))
