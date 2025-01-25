997faa386353f3f94d77a363801dae03533c7747
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-25 23:22:15 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3504 ms, Memory: 191760 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -3,25 +3,25 @@ from collections import deque
 input = sys.stdin.readline
 def solve():
     n, m = map(int, input().split())
-    g = [input() for _ in range(n)]
-    v = set()
+    g = [list(map(int, input().rstrip())) for _ in range(n)]
+    v = [[[0]*2 for _ in range(m)] for _ in range(n)]
+    v[0][0][0] = 1
     q = deque()
-    v.add((0,0,0))
-    q.append((0,0,1,0))
+    q.append((0,0,0))
+    dist = -1
     while q:
-        x, y, dist, break_wall = q.popleft()
-        if (x, y) == (n-1, m-1): break
+        x, y, break_wall = q.popleft()
+        if (x, y) == (n-1, m-1):
+            dist = v[x][y][break_wall]
+            break
         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < m:
-                if g[nx][ny] == '0': 
-                    if (nx, ny, break_wall) in v: continue
-                    q.append((nx, ny, dist+1, break_wall))
-                    v.add((nx, ny, break_wall))
-                elif break_wall == 0: 
-                    if (nx, ny, 1) in v: continue
-                    q.append((nx, ny, dist+1, 1))
-                    v.add((nx, ny, 1))
-    else: dist = -1
+                if g[nx][ny] == 0 and v[nx][ny][break_wall] == 0: 
+                    q.append((nx, ny, break_wall))
+                    v[nx][ny][break_wall] = v[x][y][break_wall]+1
+                elif g[nx][ny] == 1 and break_wall == 0 and v[nx][ny][1] == 0:
+                    q.append((nx, ny, 1))
+                    v[nx][ny][1] = v[x][y][0]+1
     print(dist)
 solve()
