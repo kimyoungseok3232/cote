0d74caecf59187d6d5df6cc5d0ea9b0e89effa79
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-25 22:57:24 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 6272 ms, Memory: 300800 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+g = [input() for _ in range(n)]
+v = set()
+q = deque()
+v.add((0,0,0))
+q.append((0,0,1,0))
+while q:
+    x, y, dist, break_wall = q.popleft()
+    if (x, y) == (n-1, m-1): break
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m:
+            if g[nx][ny] == '0': 
+                if (nx, ny, break_wall) in v: continue
+                q.append((nx, ny, dist+1, break_wall))
+                v.add((nx, ny, break_wall))
+            elif break_wall == 0: 
+                if (nx, ny, 1) in v: continue
+                q.append((nx, ny, dist+1, 1))
+                v.add((nx, ny, 1))
+else: dist = -1
+print(dist)
