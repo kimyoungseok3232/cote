7fa9dc69d826efd174aeff89cb25b6d4c460c787
kimyoungseok3232 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-01-31 21:18:25 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -2,10 +2,10 @@ for _ in range(int(input())):
     d = {}
     for _ in range(int(input())):
         k = input().split()[1]
-        d[k] = d.get(k, 0) + 1
+        d[k] = d.get(k, 1) + 1
 
     total = 1
     for count in d.values():
-        total *= (count + 1)
+        total *= count
 
     print(total - 1)
