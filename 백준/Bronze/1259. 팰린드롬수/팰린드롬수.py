080e80e39c717d73b2061108807e7217606e55d4
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-03-09 19:55:02 +0900

    [Bronze I] Title: 팰린드롬수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,7 +1,5 @@
 while (n:=input())!='0':
     l = len(n)//2
-    for i in range(l):
-        if n[i] != n[-i-1]:
-            print('no')
-            break
-    else: print('yes')
+    if n[:l] == n[::-1][:l]:
+        print('yes')
+    else:print('no')
