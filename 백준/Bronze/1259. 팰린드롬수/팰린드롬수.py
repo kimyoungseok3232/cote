eea7dd7bd00fe581f2ee625b38a5caefaea29733
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-03-09 20:11:28 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -2,4 +2,4 @@ import sys
 l=open(0).readlines()
 for i in range(len(l)-1):
 	a=l[i].replace("\n","")
-	print("yes" if sys.intern(a)==sys.intern(a[::-1]) else "no")
+	print("yes" if a==a[::-1] else "no")
