e6be871e161a68d541a50bb7053f56d74b717c2c
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-03-30 01:41:53 +0900

    [level 2] Title: 전화번호 목록, Time: 511.65 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,7 @@
 def solution(phone_book):
     answer = True
     phone_book.sort(reverse=True, key=len)
+    print('asd' in {'asd':1})
     visited = set()
     for num in phone_book:
         if num in visited:
@@ -9,4 +10,17 @@ def solution(phone_book):
         for i in range(1,len(num)+1):
             visited.add(num[:i])
             
+    return answer
+
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
     return answer
