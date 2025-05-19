21fb607fc15804a912c7ae59dddb1b8db90faf55
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-05-20 00:50:40 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 180.72 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -4,26 +4,21 @@ def gcd(a,b):
     return a
 
 def solution(arrayA, arrayB):
-    answer = []
+    answer = [0]
+    
     gcd_val = arrayA[0]
     for val in arrayA:
         gcd_val = gcd(val, gcd_val)
-
-    if gcd_val > 1:
-        for val in arrayB:
-            if val%gcd_val: continue
-            break
-        else: answer.append(gcd_val)
+    
+    if all(val % gcd_val for val in arrayB):
+        answer.append(gcd_val)
     
     gcd_val = arrayB[0]
     for val in arrayB:
         gcd_val = gcd(val, gcd_val)
+        
+    if all(val % gcd_val for val in arrayA):
+        answer.append(gcd_val)
     
-    if gcd_val > 1:
-        for val in arrayA:
-            if val%gcd_val: continue
-            break
-        else: answer.append(gcd_val)
 
-    if answer: return max(answer)
-    return 0
+    return max(answer)
