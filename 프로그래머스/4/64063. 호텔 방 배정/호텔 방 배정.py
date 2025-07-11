8190f36b8130e7686b08985d9db7406adb2dea9b
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-07-11 15:47:48 +0900

    [level 4] Title: 호텔 방 배정, Time: 207.32 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -3,21 +3,15 @@ def solution(k, room_number):
     visited = {}
     for n in room_number:
         if n not in visited:
-            visited[n] = [[n]]
-            
-            if n+1 in visited:
-                visited[n] = visited[n+1]
-                
-            if n-1 in visited:
-                visited[n-1][0] = visited[n][0]
-                
+            visited[n] = [n]
         else:
+            tmp = n
             while n in visited:
+                visited[tmp] = visited[n]
                 tmp = n
-                n = visited[n][0][0] + 1
-                if n in visited:
-                    visited[tmp][0] = visited[n][0]
+                n = visited[n][0] + 1
+
             visited[n] = visited[n-1]
-            visited[n][0][0] = n
+            visited[n][0] = n
         answer.append(n)
     return answer
