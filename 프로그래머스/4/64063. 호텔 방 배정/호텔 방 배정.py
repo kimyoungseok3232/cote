02281bf5d42fd5662b7c3a32b49176d3aa8b64b9
김영석 <91772398+kimyoungseok3232@users.noreply.github.com>
2025-07-11 16:09:00 +0900

    [level 4] Title: 호텔 방 배정, Time: 795.33 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,47 +1,16 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-import sys
-sys.setrecursionlimit(20000)
-
 def solution(k, room_number):
     answer = []
-    p = {}
-
-    def find(a):
-        if a not in p:
-            p[a] = a
-            return a
-        if p[a] == a: return a
-        p[a] = find(p[a])
-        return p[a]
-
-    def union(x):
-        answer.append(x)
-        p[x] = find(x + 1)
-
-    for request in room_number:
-        union(find(request))
-
+    visited = {}
+    for n in room_number:
+        if n not in visited:
+            visited[n] = n
+        else:
+            while n in visited:
+                tmp = n
+                n = visited[n] + 1
+                if n in visited:
+                    visited[tmp] = visited[n]
+            visited[n] = visited[n-1]
+            visited[n] = n
+        answer.append(n)
     return answer
